40eaf792a0a9ef927ffea7531691550ac00f1ecc
YASH RAJ SINGH RATHORE <149403759+yashrajbuilds@users.noreply.github.com>
2026-05-09 18:52:45 +0530

    Delete booking sys directory

--- booking sys/backend/routes/expertRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getExperts, getExpertById } = require('../controllers/expertController');
-
-const router = express.Router();
-
-router.get('/', getExperts);
-router.get('/:id', getExpertById);
-
-module.exports = router;
